6db5d4165526757d0a8c7d0959c054108dbb1abc
nemmanue <emmanuel.ndinga@ynov.com>
2024-11-14 15:13:05 +0100

    exo2

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strings"
+	"unicode"
 )
 
-func main() {
-	// Get the arguments passed to the program
-	args := os.Args[1:]
+// CapitalizeWords met en maj la premiere lettre et le reste en miniscule
+func CapitalizeWords(s string) string {
+	// Séparer la chaîne en mots
+	words := strings.Fields(s)
 
-	// Print the number of arguments
-	fmt.Println(len(args))
+	// il faut parcourir chaque mot pour modifier sa première lettre
+	for i, word := range words {
+		// Mettre la première lettre en majuscule et les autres en minuscule
+		words[i] = string(unicode.ToUpper(rune(word[0]))) + strings.ToLower(word[1:])
+	}
+
+	// Rejoindre les mots en une seule chaîne et la retourner
+	return strings.Join(words, " ")
+}
+
+func main() {
+	original := "bonjour je suis un eleve d'ynov, comment allez vous, etc"
+	result := CapitalizeWords(original)
+	fmt.Println(result)
 }
